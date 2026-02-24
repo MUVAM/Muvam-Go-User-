3cbe513050dbf37a8a600605fd1b0cdd31819590
saintdmix <josephchukwuebuka10gmail.com>
2026-02-24 20:37:42 +0000

    navigatetowallet

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -5235,7 +5235,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
                                                               // );
 
-                                                              _navigateToWallet;
+                                                              _navigateToWallet();
                                                             },
                                                             child: Container(
                                                               height: 48.h,
